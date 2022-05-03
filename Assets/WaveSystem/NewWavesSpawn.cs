26004e26160d05bcbc66836af363fe561ce2249c
LucaSchifano <luca.schifano005@gmail.com>
2022-05-03 15:29:10 +0200

    New Wave System

--- Assets/WaveSystem/NewWavesSpawn.cs
+++ Assets/WaveSystem/NewWavesSpawn.cs
@@ -13,37 +13,97 @@ public class NewWavesSpawn : MonoBehaviour
     public Transform target;
     
     [Header("Enemy")]
-    public NewTestEnemy enemy;
+    //public NewTestEnemy enemy;
     public List<float> listSphere = new List<float>();
 
     [Header("Other")] public float minDistance; 
     public bool spawn;
+    
+    
+    
+    [Header("Waves")]
+    private int wavesCount = 0;
+    private bool waveEnded;
+    [HideInInspector] public NewWaves currentWave;
+    [SerializeField] private List<NewWaves> _wavesList = new List<NewWaves>();
+    
 
     private void Update()
     {
         if (spawn || Input.GetKeyDown((KeyCode.Return)))
         {
             spawn = false;
-            spawnEnemy(enemy);
+            waveEnded = false;
+            currentWave = _wavesList[wavesCount];
+
+            StartCoroutine(spawnEnemies());
         }
-    }
 
-
-    private void spawnEnemy(NewTestEnemy lenemy)
-    {
-        Vector3 spawnDistance = transform.position + (Random.onUnitSphere * lenemy.distanceSpawn);
-        spawnDistance = new Vector3(spawnDistance.x, 0, spawnDistance.z);
-
-        while (Vector3.Distance(spawnDistance, player.position) < minDistance)
+        if (waveEnded)
         {
-            spawnDistance = transform.position + (Random.onUnitSphere * lenemy.distanceSpawn);
-            spawnDistance = new Vector3(spawnDistance.x, 0, spawnDistance.z);
+            if (wavesCount == _wavesList.Count - 1)
+            {
+                Debug.Log("end");
+            }
+            else
+            {
+                wavesCount++;
+                spawn = true;
+            }
         }
-
-        var enemyObject = Instantiate(lenemy.gameObject, spawnDistance, transform.rotation);
-        enemyObject.GetComponent<NewTestEnemy>().target = target;
-
     }
+    
+    
+    IEnumerator spawnEnemies()
+    {
+        yield return new WaitForSeconds(currentWave.delayBefore);
+        
+        for (int i = 0; i < currentWave.listEnemy.Count; i++)
+        {
+            spawned();
+            yield return new WaitForSeconds(currentWave.delayBetwSpawn);
+        }
+        
+        yield return new WaitForSeconds(currentWave.delayAfter);
+        
+        waveEnded = true;
+        
+    }
+
+
+    private void spawned()
+    {
+        var wList = _wavesList[wavesCount];
+        var lenemy = wList.listEnemy[wList.wavesCounter].enemy;
+
+        for (int i = 0; i < lenemy.Count; i++)
+        {
+            Vector3 spawnDistance = RandomCircle(transform.position, lenemy[i].distanceSpawn);
+
+            while (Vector3.Distance(spawnDistance, player.position) < minDistance)
+            {
+                spawnDistance = RandomCircle(transform.position, lenemy[i].distanceSpawn);
+            }
+
+            var enemyObject = Instantiate(lenemy[i].gameObject, spawnDistance,
+                transform.rotation);
+            enemyObject.GetComponent<NewTestEnemy>().target = target;
+
+            
+        }
+        wList.wavesCounter++;
+    }
+    
+    Vector3 RandomCircle(Vector3 center, float radius)
+    {
+        float ang = UnityEngine.Random.value * 360;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        pos.y = center.y;
+        return pos;
+    }
+   
 
 
     private void OnDrawGizmosSelected()
@@ -54,5 +114,23 @@ public class NewWavesSpawn : MonoBehaviour
         }
         
         Gizmos.DrawWireSphere(player.position, minDistance);
+    }
+    
+    
+    [System.Serializable]
+    public class NewWaves
+    {
+        public float delayBefore;
+        public float delayBetwSpawn;
+        public float delayAfter;
+        
+        public List<enemyList> listEnemy = new List<enemyList>();
+        [HideInInspector] public int wavesCounter;
+    }
+    
+    [System.Serializable]
+    public class enemyList
+    {
+        public List<NewTestEnemy> enemy = new List<NewTestEnemy>();
     }
 }
