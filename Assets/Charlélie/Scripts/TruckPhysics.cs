40ec3b2612e596fc1116f9b39d35f5d31dbadc04
Str00ke <charlelie.garrigues@edu.devinci.fr>
2022-05-12 10:22:44 +0200

    Fix truck

--- Assets/Charlélie/Scripts/TruckPhysics.cs
+++ Assets/Charlélie/Scripts/TruckPhysics.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class Testet : NetworkBehaviour
+public class TruckPhysics : NetworkBehaviour
 {
 
     [Header("Front wheels")]
