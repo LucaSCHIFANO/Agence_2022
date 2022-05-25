c320a320b08269e950aa3e8369109f4ab23064e6
LucaSchifano <luca.schifano005@gmail.com>
2022-05-25 11:57:36 +0200

    Generator upgrade

--- Assets/Luca/Generator/Generator.cs
+++ Assets/Luca/Generator/Generator.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI.Extensions;
 
@@ -13,15 +14,16 @@ public class Generator : NetworkBehaviour
     [SerializeField] private List<Transform> listSommets = new List<Transform>();
     [SerializeField] private List<UILineRenderer> myLines = new List<UILineRenderer>();
     [SerializeField] private float lineThinkness;
+    [SerializeField] private float numberOfPoint;
     [SerializeField] private List<float> colorDistance = new List<float>(); // green then orange then red
-    [SerializeField] private List<TextMeshProUGUI> textList = new List<TextMeshProUGUI>(); // green then orange then red
-    [SerializeField] private List<GameObject> textButtonOverCloke = new List<GameObject>(); // green then orange then red
+    [SerializeField] private List<TextMeshProUGUI> textList = new List<TextMeshProUGUI>(); // att def spd
+    [SerializeField] private List<GameObject> textButtonOverCloke = new List<GameObject>(); // att def spd
 
     [Header("Pourcentage")]
-    [SerializeField] private float minimumDist = 0; // 0 au plus pret du sommet
-    [SerializeField] private float maximumDist = 850; // ~840 au plus pret du sommet
-    [SerializeField] private float overclokePourcent; // ~840 au plus pret du sommet
-    [SerializeField] private float overclokePourcentOther; // ~840 au plus pret du sommet
+    private float minimumDist = 0; // 0 au plus pret du sommet
+    private float maximumDist = 850; // ~840 au plus loin du sommet
+    [SerializeField] private float overclokePourcent; // pourcent en overcloke
+    [SerializeField] private float overclokePourcentOther; // pourcent quand autre en overcloke
 
     [HideInInspector] public List<float> pourcentageList = new List<float>(); // 0 atk 1 def 2 spd
 
@@ -48,6 +50,8 @@ public class Generator : NetworkBehaviour
 
     private void Start()
     {
+        maximumDist = Vector2.Distance(listSommets[1].position, listSommets[2].position);
+
         for (int i = 0; i < 3; i++)
         {
             pourcentageList.Add(0);    
@@ -60,6 +64,7 @@ public class Generator : NetworkBehaviour
     public void onClickTriangle(bool boul)
     {
         if(boul) upgradePoint.transform.position = Input.mousePosition;
+        
         for (int i = 0; i < myLines.Count; i++)
         {
             myLines[i].gameObject.SetActive(true);
@@ -70,22 +75,22 @@ public class Generator : NetworkBehaviour
             myLines[i].transform.position = upgradePoint.transform.position;
             
             float distTop = Vector3.Distance(upgradePoint.transform.position, listSommets[i].position);
-            if (distTop < 10)
+            Vector3 vectorTop = listSommets[i].position - upgradePoint.transform.position;
+
+            if (distTop < 10 || distTop < 10)
             {
                 myLines[i].gameObject.SetActive(false);
-                return;
             }
             
-            var vectorTop = listSommets[i].position - upgradePoint.transform.position;
-            
-            int numberOfPoint = (int) (distTop / lineThinkness);
             myLines[i].LineThickness = lineThinkness;
 
             var pointlist = new List<Vector2>();
             for (int j = 1; j <= numberOfPoint; j++)
             {
-                pointlist.Add(new Vector2((vectorTop.x / numberOfPoint) * j, (vectorTop.y / numberOfPoint) * j));
+                pointlist.Add(Vector2.zero);
             }
+
+            pointlist[pointlist.Count - 1] = new Vector2(vectorTop.x, vectorTop.y);
 
             myLines[i].Points = pointlist.ToArray();
             
